f6e0a86a4b86ad91cd97cb0c990651fc1ee6a229
Lee Smith <lee.smith@appogee.net>
2011-04-30 23:05:57 -0500

    Rails 3 development branch

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source "http://rubygems.org"
+
+gem 'decent_exposure'
+gem 'devise'
+gem 'haml-rails'
+gem 'pg'
+gem 'rails'
+gem 'will_paginate', '3.0.pre2'
+
+group :development, :test do
+  gem 'capybara'
+  gem 'cucumber-rails'
+  gem 'database_cleaner'
+  gem 'factory_girl_rails'
+  gem 'ffaker'
+  gem 'launchy'
+  gem 'pickler'
+  gem 'rspec-rails'
+  gem 'ruby-debug'
+  gem 'shoulda'
+  gem 'spork'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TicketMule::Application.config.secret_token = '1d4cdf1048b1a85e9fb44b50410ec7846f0372f164e8cb2a5dd29f5d5a03cde0b9ff42bac7e6a4a7faf3af33581e1cdfdde756c7c357410e25de7b52df436a40'
